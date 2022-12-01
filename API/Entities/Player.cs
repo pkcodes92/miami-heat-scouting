5e79ac1d9928c2ad440f4083f912d786cfcfd56e
pkcodes92 <pkrish19@outlook.com>
2022-12-01 16:48:38 -0500

    Having the latest changes made

--- API/Entities/Player.cs
+++ API/Entities/Player.cs
@@ -9,5 +9,13 @@ namespace API.Entities
         public string LastName { get; set; } = null!;
         public DateTime BirthDate { get; set; }
         public int? PositionKey { get; set; }
+        public int? AgentKey { get; set; }
+        public int? FreeAgentYear { get; set; }
+        public decimal? Height { get; set; }
+        public decimal? Weight { get; set; }
+        public int? YearsOfService { get; set; }
+        public decimal? Wing { get; set; }
+        public decimal? BodyFat { get; set; }
+        public decimal? StandingReach { get; set; }
     }
 }
