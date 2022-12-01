0eaab23d7f59d7a83a4298bbd5017baa231c6d4e
pkcodes92 <pkrish19@outlook.com>
2022-12-01 16:58:19 -0500

    Having all of the player entity properties being added

--- API/Entities/Player.cs
+++ API/Entities/Player.cs
@@ -17,5 +17,27 @@ namespace API.Entities
         public decimal? Wing { get; set; }
         public decimal? BodyFat { get; set; }
         public decimal? StandingReach { get; set; }
+        public decimal? CourtRunTime { get; set; }
+        public decimal? VerticalJumpNoStep { get; set; }
+        public decimal? VerticalJumpMax { get; set; }
+        public decimal? HandWidth { get; set; }
+        public decimal? HandLength { get; set; }
+        public string URLPhoto { get; set; } = null!;
+        public bool ActiveAnalysisFlag { get; set; }
+        public int? LeagueCustomGroupKey { get; set; }
+        public int? BboPlayerKey { get; set; }
+        public DateTime InsertDateTime { get; set; }
+        public DateTime? UpdateDateTime { get; set; }
+        public string AgentName { get; set; } = null!;
+        public string AgentPhone { get; set; } = null!;
+        public string CommittedTo { get; set; } = null!;
+        public string Handedness { get; set; } = null!;
+        public int? GLPlayerKey { get; set; }
+        public int? PlayerStatusKey { get; set; }
+        public string HeightSource { get; set; } = null!;
+        public string WeightSource { get; set; } = null!;
+        public string WingSource { get; set; } = null!;
+        public string BodyFatSource { get; set; } = null!;
+        public string StandingReachSource { get; set; } = null!;
     }
 }
