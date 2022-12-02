0cb2df8ec429e35d09e8b3cbf8135e90fae6a395
pkcodes92 <pranav.krishnamurthy@emovis.us>
2022-12-02 14:27:14 -0500

    Having the necessary progress with Issue #2

--- API/Data/ScoutContext.cs
+++ API/Data/ScoutContext.cs
@@ -55,6 +55,8 @@ namespace API.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            // TODO: Have all the model column bindings accordingly.
             this.OnModelCreatingPartial(modelBuilder);
         }
 
