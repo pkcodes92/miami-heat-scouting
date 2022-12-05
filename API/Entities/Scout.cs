ce1741d8f9be86d5edd1faeca063edc86a427953
Pranav Krishnamurthy <74801920+pkcodes92@users.noreply.github.com>
2022-12-05 15:53:58 -0500

    Having the various changes for the scout and scouting reporting entities accordingly (#6)

---
+++ API/Entities/Scout.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="Scout.cs" company="Miami Heat">
+// Copyright (c) Miami Heat. All rights reserved.
+// </copyright>
+
+namespace API.Entities
+{
+    /// <summary>
+    /// This class represents the Scout entity.
+    /// </summary>
+    public partial class Scout
+    {
+        /// <summary>
+        /// Gets or sets the scout key - the primary key.
+        /// </summary>
+        public int ScoutKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the scout first name.
+        /// </summary>
+        public string ScoutFirstName { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the scout last name.
+        /// </summary>
+        public string ScoutLastName { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the date/time the record was created.
+        /// </summary>
+        public DateTime Created { get; set; }
+    }
+}
