a8f38f1f9b6bdc752ab35403aa569412651ced74
pkcodes92 <pkrish19@outlook.com>
2022-12-05 21:00:13 -0500

    Having the necessary API Response model added

---
+++ API.Data/Models/ApiResponse.cs
@@ -0,0 +1,10 @@
+namespace API.Data.Models
+{
+    public class ApiResponse
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether or not the response is successful.
+        /// </summary>
+        public bool Success { get; set; }
+    }
+}
