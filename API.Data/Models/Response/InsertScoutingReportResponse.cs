e8c8fed9d76dfbc4f3ae53f9ba5502eb7d9e3e30
pkcodes92 <pkrish19@outlook.com>
2022-12-05 21:08:17 -0500

    Having the necessary response model

---
+++ API.Data/Models/Response/InsertScoutingReportResponse.cs
@@ -0,0 +1,10 @@
+namespace API.Data.Models.Response
+{
+    public class InsertScoutingReportResponse : ApiResponse
+    {
+        /// <summary>
+        /// Gets or sets the scouting report ID.
+        /// </summary>
+        public int ScoutingReportId { get; set; }
+    }
+}
